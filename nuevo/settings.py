6dbf570e07da9bcc4a45763d16fe23f777a33f8b
Leo González <leoo.matiass.gonzalezz@gmail.com>
2024-01-02 00:48:58 -0300

    x

--- nuevo/settings.py
+++ nuevo/settings.py
@@ -130,14 +130,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR),'static'),)
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = [BASE_DIR / 'static',]
+STATIC_ROOT = '/home/nombre/grupo_unido/staticfiles'
 #acá tienen que poner su ruta 
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/home/nombre/repo1/media'
+MEDIA_ROOT = '/home/nombre/grupo_unido/media'
 #acá tienen que poner su ruta 
 
 
